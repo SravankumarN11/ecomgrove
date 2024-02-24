1410fed5b92e75033d6aec39cb1728e922b47e3a
sravankumar Nagarampalli <120583448+SravankumarN11@users.noreply.github.com>
2024-02-24 10:39:49 +0530

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,14 @@
+function scroll3(){
+    alert("Buy one chocolate for your partner to get more interact with your partner");
+   /* alert("Bokka ley e matalu anni vinnavu anuko jeevitam sankanakipothundhi")
+   let response= confirm("Still you are buy chocolate for your partner?")
+   if(response){
+    alert("Sare Nee istam");
+   }
+   else{
+    alert("Come on ra Bhai");
+    var none=document.getElementById("ok");
+    none.style.display="none";
+   }*/
+
+}
